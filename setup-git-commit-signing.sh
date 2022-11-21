b25755a9480d2562318b6eaa964732e49258ab83
Jacob Heider <jacob@tea.xyz>
2022-11-21 17:58:40 -0500

    fix someone's bug

--- setup-git-commit-signing.sh
+++ setup-git-commit-signing.sh
@@ -44,7 +44,7 @@ tea +crates.io/bpb which -s bpb
 if ! test -f ~/.bpb_keys.toml; then
   #TODO probably we should ask `git` first
 
-  tea +charm.sh/gum format "k first up, we’ll configure your GPG key"
+  tea +charm.sh/gum gum format "k first up, we’ll configure your GPG key"
 
   user=$(tea +charm.sh/gum input "what’s your name?")
   email=$(tea +charm.sh/gum input "what’s your email?")
